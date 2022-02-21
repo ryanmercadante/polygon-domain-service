db391f6562f47d36e2b8bf2f4a20eb266a53af13
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-02-21 16:05:54 -0500

    add functions to set/get records

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -7,12 +7,17 @@ contract Domains {
   // A "mapping" data type to store their names
   mapping(string => address) public domains;
 
+  // Store values
+  mapping(string => string) public records;
+
   constructor() {
     console.log("This contract registers domains!");
   }
 
   // A register function that adds their names to our mapping
   function register(string calldata name) public {
+    // Check that the name is unregistered
+    require(domains[name] == address(0), "Name is already registered.");
     domains[name] = msg.sender;
     console.log("%s has registered a domain!", msg.sender);
   }
@@ -20,5 +25,15 @@ contract Domains {
   // This will give us the domain owner's address
   function getAddress(string calldata name) public view returns (address) {
     return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+    // Check that the owner is the transaction sender
+    require(domains[name] == msg.sender, "You don't own this domain");
+    records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns (string memory) {
+    return records[name];
   }
 }
--- scripts/run.ts
+++ scripts/run.ts
@@ -2,7 +2,7 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [owner] = await ethers.getSigners();
+  const [owner, randomPerson] = await ethers.getSigners();
   const domainContractFactory = await ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
@@ -10,11 +10,16 @@ async function main() {
   console.log("Contract deployed to:", domainContract.address);
   console.log("Contract deployed by:", owner.address);
 
-  const tx = await domainContract.register("doom");
+  let tx = await domainContract.register("doom");
   await tx.wait();
 
   const domainOwner = await domainContract.getAddress("doom");
   console.log("Owner of domain:", domainOwner);
+
+  tx = await domainContract
+    .connect(randomPerson)
+    .setRecord("doom", "haha my domain now!");
+  await tx.wait();
 }
 
 async function runMain() {
