4fc23d943a06ea0e515044babca6553a58c0d6f1
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-02-21 14:27:17 -0500

    add mapping for domains, start register func, getAddress func

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -4,8 +4,21 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
-    constructor() {
-      console.log("THIS IS MY DOMAINS CONTRACT. NICE!");
-    }
+  // A "mapping" data type to store their names
+  mapping(string => address) public domains;
 
+  constructor() {
+    console.log("This contract registers domains!");
+  }
+
+  // A register function that adds their names to our mapping
+  function register(string calldata name) public {
+    domains[name] = msg.sender;
+    console.log("%s has registered a domain!", msg.sender);
+  }
+
+  // This will give us the domain owner's address
+  function getAddress(string calldata name) public view returns (address) {
+    return domains[name];
+  }
 }
--- scripts/run.ts
+++ scripts/run.ts
@@ -2,10 +2,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
+  const [owner] = await ethers.getSigners();
   const domainContractFactory = await ethers.getContractFactory("Domains");
   const domainContract = await domainContractFactory.deploy();
   await domainContract.deployed();
+
   console.log("Contract deployed to:", domainContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  const tx = await domainContract.register("doom");
+  await tx.wait();
+
+  const domainOwner = await domainContract.getAddress("doom");
+  console.log("Owner of domain:", domainOwner);
 }
 
 async function runMain() {
