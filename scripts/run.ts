4d149c6daf5d29c4cf203180b2c9137a9d5b5bf3
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-03-14 22:04:54 -0400

    update run script, require node >= 16.0.0

--- scripts/run.ts
+++ scripts/run.ts
@@ -2,24 +2,29 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [owner, randomPerson] = await ethers.getSigners();
+  // const [owner, randomPerson] = await ethers.getSigners();
   const domainContractFactory = await ethers.getContractFactory("Domains");
-  const domainContract = await domainContractFactory.deploy();
+  const domainContract = await domainContractFactory.deploy("ninja");
   await domainContract.deployed();
 
   console.log("Contract deployed to:", domainContract.address);
-  console.log("Contract deployed by:", owner.address);
+  // console.log("Contract deployed by:", owner.address);
 
-  let tx = await domainContract.register("doom");
+  const tx = await domainContract.register("mortal", {
+    value: ethers.utils.parseEther("0.11"),
+  });
   await tx.wait();
 
-  const domainOwner = await domainContract.getAddress("doom");
+  const domainOwner = await domainContract.getAddress("mortal");
   console.log("Owner of domain:", domainOwner);
 
-  tx = await domainContract
-    .connect(randomPerson)
-    .setRecord("doom", "haha my domain now!");
-  await tx.wait();
+  const balance = await ethers.provider.getBalance(domainContract.address);
+  console.log("Contract balance:", ethers.utils.formatEther(balance));
+
+  // tx = await domainContract
+  //   .connect(randomPerson)
+  //   .setRecord("doom", "haha my domain now!");
+  // await tx.wait();
 }
 
 async function runMain() {
